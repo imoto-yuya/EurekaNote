275ed367b2f9ae0a8258703aef34c04f2baf953b
Yuya Imoto <imoto.yuya@icloud.com>
2018-08-22 00:15:42 +0900

    design basic input view's layout

--- EurekaNote/ViewController.swift
+++ EurekaNote/ViewController.swift
@@ -10,14 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var inputTextView: UITextView!
+    @IBOutlet weak var categorySegmentedControl: UISegmentedControl!
+
+    @IBAction func canncelButton(_ sender: Any) {
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // キーボード用のツールバーを表示
+        inputTextView.inputAccessoryView = makeToolbar()
+        // キーボードを表示する
+        inputTextView.becomeFirstResponder()
+    }
+
+    override func viewWillLayoutSubviews() {
+        updateViewLayout()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func updateViewLayout() {
+        let screenWidth = self.view.bounds.width
+        let screenHeight = self.view.bounds.height
+        let inputTextViewHeight = screenHeight/5
+
+        // ラベルの位置を指定
+        titleLabel.center.x = screenWidth/2
+        titleLabel.frame.origin.y = inputTextViewHeight - titleLabel.frame.size.height - inputTextViewHeight/8
+
+        // テキストビューのサイズと位置を指定
+        inputTextView.frame.size = CGSize(width: screenWidth*0.8, height: screenHeight*0.2)
+        inputTextView.center.x = screenWidth/2
+        inputTextView.frame.origin.y = inputTextViewHeight
+
+        // カテゴリ選択欄のサイズと位置を指定
+        categorySegmentedControl.frame.size.width = inputTextView.frame.size.width
+        categorySegmentedControl.center.x = screenWidth/2
+        categorySegmentedControl.frame.origin.y = inputTextView.frame.origin.y + inputTextView.frame.size.height + 20
+    }
+
+    @objc func saveProcess(_ sender: UIButton) {
+    }
+
+    func makeToolbar() -> UIToolbar {
+        // キーボード用のツールバーを作成
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 40))
+        // スタイルを設定
+        toolbar.barStyle = UIBarStyle.default
+        // 画面幅に合わせてサイズを変更
+        toolbar.sizeToFit()
+        // ツールバーに表示するボタンを設定
+        toolbar.items = [
+            UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: self, action: nil),
+            UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: self, action: #selector(saveProcess))
+        ]
+        return toolbar
     }
 
 
